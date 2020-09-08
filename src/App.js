0013b656c70754ac81bbb58294cf5255dc6db0bc
Ankit <ankitksh81@gmail.com>
2020-09-09 01:36:42 +0530

    Added Login functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
     <div className="app">
-      
+      <Login />
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+    return (
+        <div className="login">
+            <img src="http://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo" />
+            <a>Login with Spotify</a>
+        </div>
+    );
+}
+
+export default Login;
