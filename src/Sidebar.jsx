d99233adac7f72da6e60be3adfd288238f857613
Ankit <ankitksh81@gmail.com>
2020-09-12 22:26:35 +0530

    Added sidebar options & material ui

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Sidebar.css';
+import SidebarOption from './SidebarOption';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <img className="sidebar__logo" src="http://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo" />
+            <SidebarOption title="Home" />
+            <SidebarOption title="Search" />
+            <SidebarOption title="Your Library" />
         </div>
     );
 }
