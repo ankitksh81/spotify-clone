04eb3f06cde0abdd4ef5a1de3329cc9ba7648f55
Ankit <ankitksh81@gmail.com>
2020-09-12 21:53:59 +0530

    Formated body & sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -4,7 +4,7 @@ import './Sidebar.css';
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>I am the Sidebar</h1>
+            <img className="sidebar__logo" src="http://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo" />
         </div>
     );
 }
