0d90537677c5c38d81a8b4660625ef41eee990e9
Ankit <ankitksh81@gmail.com>
2020-09-26 22:52:50 +0530

    added icons and css properties

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,3 +1,4 @@
+import { PlayCircleOutline, Repeat, Shuffle, SkipNext, SkipPrevious } from '@material-ui/icons';
 import React from 'react';
 import './Footer.css';
 
@@ -8,7 +9,11 @@ function Footer() {
                 <h1>Album and song details</h1>
             </div>
             <div className="footer__center">
-                <h1>Player controls</h1>
+                <Shuffle className="footer__green" />
+                <SkipPrevious className="footer__icon" />
+                <PlayCircleOutline fontSize="large" className="footer__icon" />
+                <SkipNext className="footer__icon" />
+                <Repeat className="footer__green" />
             </div>
             <div className="footer__right">
                 <h1>Volume controls</h1>
