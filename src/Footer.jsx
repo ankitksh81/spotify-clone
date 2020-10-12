79c9b8ce9794bce4cbd6c15e5e600b2fdc7810db
Ankit <ankitksh81@gmail.com>
2020-10-12 23:25:56 +0530

    added volume control and album art

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,13 +1,19 @@
-import { PlayCircleOutline, Repeat, Shuffle, SkipNext, SkipPrevious } from '@material-ui/icons';
+import { PlayCircleOutline, PlaylistPlay, Repeat, Shuffle, SkipNext, SkipPrevious, VolumeDown } from '@material-ui/icons';
 import React from 'react';
 import './Footer.css';
+import { Grid, Slider } from '@material-ui/core';
 
 function Footer() {
     return (
         <div className="footer">
             <div className="footer__left">
-                <h1>Album and song details</h1>
+                <img className="footer_albumLogo" scr="https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Usher_-_Confessions_album_cover.jpg/220px-Usher_-_Confessions_album_cover.jpg" alt="" />
+                <div className="footer_songInfo">
+                    <h4>Yeah!</h4>
+                    <p>Usher</p>
+                </div>
             </div>
+
             <div className="footer__center">
                 <Shuffle className="footer__green" />
                 <SkipPrevious className="footer__icon" />
@@ -15,8 +21,19 @@ function Footer() {
                 <SkipNext className="footer__icon" />
                 <Repeat className="footer__green" />
             </div>
+
             <div className="footer__right">
-                <h1>Volume controls</h1>
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlay />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDown />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
             </div>
         </div>
     );
