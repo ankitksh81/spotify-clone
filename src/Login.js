414dbdabfbe89431fd21c35ad5e1808166e9dc9b
ankitksh81 <ankitksh81@gmail.com>
2021-01-01 21:58:40 +0530

    minor changes

--- src/Login.js
+++ src/Login.js
@@ -5,7 +5,7 @@ import { loginUrl } from "./spotify";
 function Login() {
   return (
     <div className="login">
-      <img
+      <img className="spotify-logo"
         src="http://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt="logo"
       />
